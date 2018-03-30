92085dbe392adb8ea8a6b1c17cacd2439cd073fc
NWBeekhuis <niels.beekhuis@gmail.com>
2018-03-30 17:44:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ clear
 echo "******* Torghost installer ********"
 echo ""
 echo "=====> Installing tor bundle "
-apt-get install tor -y -qq
+sudo apt-get install tor -y -qq
 echo "=====> Installing dependencies "
-pip install stem
+sudo pip install stem
 echo "=====> Installing TorGhost "
 cp torghost /usr/bin/torghost
 chmod +x /usr/bin/torghost
